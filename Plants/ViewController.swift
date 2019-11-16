7021e9ffe8cebb334df5856c5f2842180f4d8124
Kyaw Saw Thu <cyberboykyawsawthu@gmail.com>
2019-11-16 08:01:25 +0630

    Initial Commit

---
+++ Plants/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Plants
+//
+//  Created by Riki on 11/16/19.
+//  Copyright © 2019 KyawSawThu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
